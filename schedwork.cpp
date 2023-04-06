028dd0ef05bed4a8934771887d3b1d4bf9514e77
Anna Andreu <aandreu@usc.edu>
2023-04-06 19:18:33 +0000

    finished!

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,15 +1,14 @@
 #ifndef RECCHECK
 #include <set>
-#include <iostream>
+//#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
-
 #endif
 
+// add or remove necessary headers as you please
 #include "schedwork.h"
 
 using namespace std;
@@ -21,7 +20,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+//modeled after sudoku lab - check if valid, if not backtrack
+bool schedule_helper(
+	const AvailabilityMatrix& avail,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  DailySchedule& sched,
+  size_t curr_day,
+  size_t curr_worker
+);
+  
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +45,88 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    /*const size_t total_days = avail.size(); // n
+    const size_t possible_workers = avail[0].size(); // k
+	const size_t workers_needed = sched[0].size(); // d */
 
+// SCHEDULE SOLUTION n x d
+// make a vector with spots for as many workers needed and their id's
+    std::vector<Worker_T> workers_needed(dailyNeed, INVALID_ID);
+    // ^ for columns- how many workers needed
+    
+    // vector of vectors to indicating the d workers (col)
+    //who are scheduled to work on each of the n days (rows)
+    for(unsigned int row = 0; row < avail.size(); row++){ // for loop in set up 
+        // populate schedule based on how many workers needed each day
+        sched.push_back(workers_needed);
+    }
 
-
-
+	return schedule_helper(avail, dailyNeed, maxShifts, sched, 0, 0);
 }
 
+bool schedule_helper(
+	const AvailabilityMatrix& avail,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  DailySchedule& sched,
+  size_t curr_day,
+  size_t curr_worker
+)
+{
+    //const size_t total_days = avail.size(); // n
+    const size_t total_workers = avail[0].size(); // k
+    //const size_t workers_needed = sched[0].size(); // d
+    
+    // BC end recursive cases - all complete
+    if(curr_day == sched.size()){
+        return true;
+    }
+
+		//iterate through all workers 
+    for(size_t x= 0; x < total_workers; x++){ // one loop for recursive solution
+			 // if worker on sched today already 
+				if(avail[curr_day][x]){ 
+            if(find(sched[curr_day].begin(), sched[curr_day].end(), x) != sched[curr_day].end()){
+							continue; // if already scheduled- dont do anything
+						}
+				
+				//else schedule worker 
+				sched[curr_day][curr_worker] = x;
+				//if that position is valid for this and next round
+
+				size_t shift_count = 0;
+				bool isValid = false; 
+				//go through whole sched and note how many times 
+				// current worker has been scheduled for
+				for(size_t y = 0; y < sched.size(); y++){
+					for(size_t z = 0; z < dailyNeed; z++){
+						if(sched[y][z] == x) // x would be the curr worker id
+						shift_count++;
+					}
+					}
+
+				// if shift count is less than or equal to max shifts 
+				if(shift_count <= maxShifts){
+					isValid = true; 
+				}
+				
+				if(isValid == true){
+					if(curr_worker == dailyNeed - 1){ // if reach end of colum
+						if(schedule_helper(avail, dailyNeed, maxShifts, sched, curr_day + 1, 0)){
+							return true; 
+						}
+					}
+					//if have space in the row- recurse to next worker 
+					else{
+						if(schedule_helper(avail, dailyNeed, maxShifts, sched, curr_day, curr_worker +1)){
+							return true; 
+						}
+					}
+				}
+    }
+		}
+// not valid after trying all workers- set back to OG 
+		sched[curr_day][curr_worker]= INVALID_ID;
+		return false; 
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 // For debugging
-#include <iostream>
+//#include <iostream>
 // For std::remove
 #include <algorithm> 
 #include <map>
@@ -13,16 +13,111 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordle_recursive(
+    std::string whole_input,
+    std::string floating,
+	unsigned int curr_index,
+    const std::set<std::string>& dict,
+    std::set<std::string>& words_returned
+); 
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
-    const std::string& in,
+    const std::string&in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
     // Add your code here
+    
+    //make copy so u can use in helper
+    std::string whole_input = in; 
+    std::string floating_copy = floating; 
+
+    //set of all possible words that will be returned 
+    std::set<std::string> words_returned;
+
+    //helper to recurse 
+    wordle_recursive(whole_input, floating_copy, 0, dict, words_returned);
+    return words_returned; 
 
 }
 
 // Define any helper functions here
+void wordle_recursive(
+    std::string whole_input,
+    std::string floating,
+	unsigned int curr_index,
+    const std::set<std::string>& dict,
+    std::set<std::string>& words_returned)
+{
+    char curr_letter; 
+    unsigned int to_be_filled = 0;
+    //unsigned int correct_letters = 0; 
+    
+    // count how many fixed and need to be filled letters 
+    for(unsigned int i = 0; i < whole_input.size(); ++i){
+        curr_letter = whole_input[i];
+        if(curr_letter == '-'){
+            to_be_filled++;
+        }
+        //correct_letters++;
+    }
+
+    //BC to END RECURSION 
+    if(curr_index == whole_input.size()){ // input is full
+        // if the word appears in dictionary 
+        if(dict.find(whole_input) != dict.end()){ 
+            words_returned.insert(whole_input); // add possible words 
+        }
+        return; 
+    }
+
+// RECURSIVE CASES 
+    // if hit a FIXED letter  
+    if(whole_input[curr_index] != '-'){
+        // go to next index
+        wordle_recursive(whole_input, floating, curr_index + 1, dict, words_returned); // recurse again 
+        return;
+    }
+    // else hit a '-' ,, needs to be FILLED 
+    else{
+    // if number of floating letters = to spaces to be filled
+        if(to_be_filled == floating.size()){    
+            // make the word combos 
+            for(unsigned int x = 0; x < floating.size(); ++x){
+                // set curr space to x letter of floating
+                whole_input[curr_index] = floating[x]; 
+
+                // remove that space from floating (a copy)
+                std::string updated_floating = floating.substr(0,x) + floating.substr(x+1); 
+                //recurse w new floating
+                wordle_recursive(whole_input, updated_floating, curr_index + 1, dict, words_returned);
+            }
+        }
+    
+        else{
+         // if MORE spaces to be filled than floating size 
+        //if(to_be_filled > floating.size())
+
+            for(int x = 0; x < 26; ++x){
+                // index becomes a letter a-z
+                whole_input[curr_index] = (char)(x + 97); // convert int to char
+                
+                // if curr letter (of input index) in a letter floating 
+                if(floating.find(whole_input[curr_index]) <= floating.size()){ 
+                
+                //ERASE that letter out of the floating (a copy)    
+                    std::string updated_floating = floating;
+				    updated_floating.erase(floating.find(whole_input[curr_index]), 1);
+                    //recurse w updated floating 
+                    wordle_recursive(whole_input, updated_floating, curr_index + 1, dict, words_returned);
+                }
+                else{ // if the letter is not in floating, then keep recursing 
+                    wordle_recursive(whole_input, floating, curr_index + 1, dict, words_returned);
+                }   
+            }
+        }
+    }
+
+
+}
